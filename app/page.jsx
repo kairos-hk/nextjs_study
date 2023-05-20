3ac402c4e215254fcd1104f0af728d677bae1459
kairos-hk <kalilinuxbloger@gmail.com>
2023-05-20 11:17:57 +0900

    update

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  let name = "Kairos.hk";
+  return (
+    <div>
+      <h4 className="title">Next.js Study</h4>
+      <p className="title-sub">by dev {name}</p>
+    </div>
+  )
+}

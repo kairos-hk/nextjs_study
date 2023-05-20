f46737142d51246c16902f751d3967f826d8a66a
kairos-hk <kalilinuxbloger@gmail.com>
2023-05-20 23:18:14 +0900

    update

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -2,6 +2,9 @@ export default function about(){
     return (
         <div>
             <h1 className="about-title">About</h1>
+            <div className="about-img">
+                <img src="/about.png" />
+            </div>
         </div>
     )
 }
